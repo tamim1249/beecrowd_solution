a45fce2ce5983f9368ee5d1f40ab8570a19e65bc
Tamim Mahmud <143133639+tamim1249@users.noreply.github.com>
2025-06-09 19:53:56 +0600

    Create 2840.cpp

---
+++ 2840.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double a,b,c,d,e;
+    cin>>a>>b;
+    c=a*a*a;
+    d=3.1415*c;
+    e=(4/3.0)*d;
+    int f=b/e;
+    cout<<f<<endl;
+    return 0;
+
+
+}
