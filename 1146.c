984d90ea7f650c36a71bdb7128a5460021026e38
Tamim Mahmud <143133639+tamim1249@users.noreply.github.com>
2025-06-09 19:49:30 +0600

    Create 1146.c

---
+++ 1146.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    while(1)
+    {
+       scanf("%d",&n);
+       if(n==0)
+       {
+           break;
+       }
+       for(int i=1;i<=n;i++)
+       {
+           printf("%d",i);
+           if(i<n)
+           {
+               printf(" ");
+           }
+       }
+       printf("\n");
+
+    }
+    return 0;
+}
