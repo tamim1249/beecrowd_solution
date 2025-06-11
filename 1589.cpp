cd489e9e64c521481accb3412759fe3e4e80ecb7
Tamim Mahmud <143133639+tamim1249@users.noreply.github.com>
2025-06-12 00:42:45 +0600

    Create 1589.cpp

---
+++ 1589.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        int a,b;
+        cin>>a>>b;
+        int c=a+b;
+        cout<<c<<endl;
+    }
+    return 0;
+}
